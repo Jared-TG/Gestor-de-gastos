5d60f85b0640712c4dff97ee1fb350b2b6c473fc
Jared Francisco Teran Teran <teranrex2024@gmil.com>
2026-05-22 08:32:16 -0700

    feat:pages

---
+++ src/app/pages/gastos/gastos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gastos',
+  templateUrl: './gastos.component.html',
+  styleUrls: ['./gastos.component.scss'],
+})
+export class GastosComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
