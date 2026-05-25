23be9b7c6887a3b34b3c4ccdb501a21a63dd23b2
Jared Francisco Teran Teran <teranrex2024@gmil.com>
2026-05-24 22:06:59 -0700

    feat: nav-bar

--- src/app/pages/escanear/escanear.component.ts
+++ src/app/pages/escanear/escanear.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-escanear',
   templateUrl: './escanear.component.html',
   styleUrls: ['./escanear.component.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class EscanearComponent  implements OnInit {
 
